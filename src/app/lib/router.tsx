53c12b95028071328c50f0d1125b8a10626f91fd
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-10-18 23:41:52 +0000

    fix github pages

--- src/app/lib/router.tsx
+++ src/app/lib/router.tsx
@@ -1,10 +1,10 @@
 import React from "react"
-import { createBrowserRouter } from "react-router-dom"
+import { createHashRouter } from "react-router-dom"
 import { Layout } from "../../widgets/layout"
 import { HeroPage } from "../ui/hero-page"
 import { HomePage } from "../ui/home-page"
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     element: <Layout />,
