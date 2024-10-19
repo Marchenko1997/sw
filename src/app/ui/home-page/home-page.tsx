4f25a72fe41c77c946f9f908a3ea54a976c49927
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-10-19 09:10:13 +0000

    add pagination

--- src/app/ui/home-page/home-page.tsx
+++ src/app/ui/home-page/home-page.tsx
@@ -1,10 +1,18 @@
-import { Link } from "react-router-dom"
+import { Pagination } from "@nextui-org/react"
+import { useEffect } from "react"
+import { Link, redirect, useNavigate, useSearchParams } from "react-router-dom"
 import useSWRImmutable from "swr/immutable"
 import styles from "./home-page.module.css"
 import { IData } from "./home-page.types"
 
 export const HomePage = () => {
-  const { data } = useSWRImmutable<IData>("people/")
+  const [searchParams] = useSearchParams()
+  const page = Number(searchParams.get("page")) || 1
+  const { data } = useSWRImmutable<IData>(`people/?page=${page}`)
+  const navigate = useNavigate()
+
+  const count = Number(data?.count) || 1
+  const total = Math.max(Math.floor(count / 10), page)
 
   return (
     <div className={styles.wrapper}>
@@ -15,6 +23,15 @@ export const HomePage = () => {
           </div>
         </Link>
       ))}
+      {data && (
+        <Pagination
+          total={total}
+          initialPage={page}
+          onChange={(page) => {
+            navigate(`/?page=${page}`)
+          }}
+        />
+      )}
     </div>
   )
 }
